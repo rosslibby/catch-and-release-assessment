e953555de7d8d06ea202ac65cef561c42ba57ccf
Ross Libby <dev@rosslibby.com>
2024-01-26 10:54:18 -0500

    abstract Search to its own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,93 +1,14 @@
 'use client'
 import styles from './page.module.css'
 import Country from './country'
-import {
-  ChangeEvent,
-  useCallback,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from 'react'
-import { cacheCtx, useCache, useSearch } from './cache'
+import Search from './search'
 
 export default function Home() {
-  const inputRef = useRef<HTMLInputElement>(null)
-  const { addCountry, clear, hasCountry, isEmpty } = useCache()
-  const { filter } = useSearch()
-  const { _: { setCountry } } = useContext(cacheCtx)
-  const [value, setValue] = useState<string>('')
-
-  const submit = useCallback(async () => {
-    const cached = await hasCountry(value)
-
-    if (!cached) {
-      console.log('Fetching because it is not cached')
-      const { data: { country } } = await (await fetch(`/api/country/${value}`)).json()
-      addCountry(country)
-      setCountry(country)
-    } else {
-      setCountry(cached)
-    }
-  }, [addCountry, hasCountry, setCountry, value])
-
-  useEffect(() => {
-    let current = inputRef.current || null
-    const handleEnterKey = (e: KeyboardEvent) => {
-      if (e.key === 'Enter') {
-        submit()
-      } else if (e.key.match(/[a-zA-Z]/g)?.length === 1) {
-        // filter cached results (if applicable)
-        const result = filter((e.target as HTMLInputElement).value + e.key)
-        console.log('result is', result)
-      }
-    }
-
-    if (current) {
-      current.addEventListener('keydown', handleEnterKey)
-    }
-
-    return () => {
-      current?.removeEventListener('keydown', handleEnterKey)
-      current = null
-    }
-  }, [inputRef, submit])
-
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value.toUpperCase())
-  }
-
   return (
     <main className={styles.wrapper}>
       <h1>Enter text</h1>
-      <div className={styles.search} id="search">
-        <input
-          className={styles.input}
-          id="input"
-          onChange={handleInputChange}
-          placeholder="Type a country"
-          ref={inputRef}
-          type="text"
-        />
-        <input type="hidden" id="country-code" />
-        <ul className={styles.suggestions} id="suggestions">
-        </ul>
-      </div>
+      <Search />
       <Country />
-      <div className="buttons">
-        <button
-          className={styles.button}
-          disabled={value.length !== 2 || !value.match(/[A-Z]{2}/g)}
-          id="submit"
-          onClick={submit}
-        >Submit</button>
-        <button
-          id="reset"
-          className={styles.button}
-          disabled={isEmpty}
-          onClick={clear}
-        >Refetch</button>
-      </div>
     </main>
   )
 }
