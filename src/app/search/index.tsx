ae680b6f8139ca178b97bbef09cc4769ad9a7c6a
Ross Libby <dev@rosslibby.com>
2024-01-26 12:32:32 -0500

    styling & aesthetics

--- src/app/search/index.tsx
+++ src/app/search/index.tsx
@@ -84,25 +84,28 @@ export default function Search() {
             autoComplete="hellno"
             className={styles.input}
             onChange={handleInputChange}
-            placeholder="Type a country"
+            placeholder="Enter a country code (e.g. US)"
             ref={inputRef}
             type="text"
           />
-          {loading && (
-            <Loading />
-          )}
+          <Loading loading={loading} />
+          <button
+            className={styles.searchButton}
+            disabled={value.length !== 2 || !value.match(/[A-Z]{2}/g) || loading}
+            id="submit"
+            onClick={submit}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+              <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
+            </svg>
+          </button>
+
         </div>
         {results.length > 0 && (
           <Suggestions />
         )}
       </div>
       <div className="buttons">
-        <button
-          className={styles.button}
-          disabled={value.length !== 2 || !value.match(/[A-Z]{2}/g) || loading}
-          id="submit"
-          onClick={submit}
-        >Submit</button>
         <button
           id="reset"
           className={styles.button}
--- src/app/search/loading/index.tsx
+++ src/app/search/loading/index.tsx
@@ -1,12 +1,16 @@
 import styles from './loading.module.css'
 
-export default function Loading() {
-  return (
+export default function Loading({ loading }: {
+  loading: boolean
+}) {
+  return loading ? (
     <div className={styles.ring}>
       <div />
       <div />
       <div />
       <div />
     </div>
+  ) : (
+    <span />
   )
 }
