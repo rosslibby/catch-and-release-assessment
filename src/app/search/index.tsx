b695840104511c1df8a93a968475c8d1e1f04fb1
Ross Libby <dev@rosslibby.com>
2024-01-26 21:31:23 -0500

    abstracted functionality

--- src/app/search/index.tsx
+++ src/app/search/index.tsx
@@ -1,71 +1,30 @@
 import {
-  ChangeEvent,
   useContext,
   useEffect,
   useRef,
   useState,
 } from 'react'
 import styles from './search.module.css'
-import { useSearch } from './hooks'
 import Suggestions from './suggestions'
 import Loading from './loading'
 import { appCtx, useCache } from '@/app/data'
 import SubmitButton from './submit'
-import { useApi } from './hooks'
+import { useInput } from './hooks/input'
+import { classnames } from '@/utils'
 
 export default function Search() {
   const searchRef = useRef<HTMLDivElement>(null)
   const suggestionsRef = useRef<HTMLUListElement>(null)
   const inputRef = useRef<HTMLInputElement>(null)
-  const { country, loading, message, results } = useContext(appCtx)
+  const { message } = useContext(appCtx)
+  const { changeInput, registerRefs, value } = useInput()
   const { clear, isEmpty } = useCache()
-  const { filter } = useSearch()
-  const [value, setValue] = useState<string>('')
-  const { submit } = useApi()
   const [focused, setFocused] = useState<boolean>(false)
-  const searchClassnames = [
-    styles.search,
-    ...(focused ? [styles.searchFocus] : []),
-  ].join(' ')
+  const searchClassnames = classnames([styles.search, [styles.searchFocus, focused]])
 
   useEffect(() => {
-    let current = inputRef?.current || null
-
-    if (current && country) {
-      current.value = `(${country.code}) ${country.name}`
-    }
-
-    return () => {
-      current = null
-    }
-  }, [country, inputRef, setValue])
-
-  useEffect(() => {
-    let current = inputRef.current || null
-    let suggestions = suggestionsRef.current || null
-    const handleEnterKey = (e: KeyboardEvent) => {
-      if (e.key === 'Enter') {
-        submit(value)
-      } else if (e.key === 'ArrowDown' && results.length && suggestions) {
-        (suggestions.firstChild as HTMLElement)?.focus()
-      }
-    }
-
-    if (current) {
-      current.addEventListener('keydown', handleEnterKey)
-    }
-
-    return () => {
-      current?.removeEventListener('keydown', handleEnterKey)
-      current = null
-      suggestions = null
-    }
-  }, [inputRef, results, suggestionsRef, submit])
-
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value.toUpperCase())
-    filter(e.target.value)
-  }
+    registerRefs(inputRef, suggestionsRef)
+  }, [inputRef, suggestionsRef])
 
   return (
     <div className={styles.wrapper}>
@@ -74,29 +33,26 @@ export default function Search() {
           <input
             autoComplete="hellno"
             className={styles.input}
-            onChange={handleInputChange}
+            onChange={changeInput}
             placeholder="Enter a country code (e.g. US)"
             onFocus={() => setFocused(true)}
             onBlur={() => setFocused(false)}
             ref={inputRef}
             type="text"
           />
-          <Loading loading={loading} />
 
-          <SubmitButton
-            loading={loading}
-            submit={() => submit(value)}
-            value={value}
-          />
+          <Loading />
+
+          <SubmitButton value={value} />
 
         </div>
-        {results.length > 0 && (
-          <Suggestions ref={suggestionsRef} />
-        )}
+        <Suggestions ref={suggestionsRef} inputRef={inputRef} />
       </div>
+
       {message && (
         <p>{message}</p>
       )}
+
       <div className="buttons">
         <button
           id="reset"
