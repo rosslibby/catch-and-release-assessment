0a2d1b1a3f1f7bee22bbdaee2e5fbe476d922b91
Ross Libby <dev@rosslibby.com>
2024-01-26 20:20:27 -0500

    updated CacheProvider to AppProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import CacheProvider from "./cache";
+import AppProvider from "./data";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <CacheProvider>
+        <AppProvider>
           {children}
-        </CacheProvider>
+        </AppProvider>
       </body>
     </html>
   );
