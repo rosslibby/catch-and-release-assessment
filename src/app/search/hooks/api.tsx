4f9f19f69a63d17b917f352b495940dea964763a
Ross Libby <dev@rosslibby.com>
2024-01-26 20:23:37 -0500

    abstracted Search hooks;created API hook

---
+++ src/app/search/hooks/api.tsx
@@ -0,0 +1,54 @@
+import { appCtx, useCache } from '@/app/data'
+import { useCallback, useContext } from 'react'
+
+export const useApi = () => {
+  const { addCountry, hasCountry } = useCache()
+  const { _: {
+    setCountry,
+    setLoading,
+    setMessage,
+    setResults,
+  } } = useContext(appCtx)
+
+  const submit = useCallback(async (value: string) => {
+    setMessage(null)
+    setResults([])
+    setCountry()
+    setLoading(true)
+
+    const cached = await hasCountry(value)
+
+    if (!cached) {
+      const result = await fetch(`/api/country/${value.toUpperCase()}`)
+      console.log(result)
+      if (result.status === 200) {
+        const { data } = await result.json()
+        console.log(data)
+        addCountry(data)
+        setCountry(data)
+      } else {
+        const { message } = await result.json()
+        setMessage(message)
+      }
+      // const { data } = await (await fetch(`/api/country/${value.toUpperCase()}`)).json()
+
+      // if (data) {
+      //   addCountry(data)
+      //   setCountry(data)
+      // } else {
+      //   setMessage(`No results found for ${value.toUpperCase()}`)
+      // }
+    } else {
+      setCountry(cached)
+    }
+
+    setLoading(false)
+  }, [
+    setCountry,
+    setLoading,
+    setMessage,
+    setResults,
+  ])
+
+  return { submit }
+}
---
+++ src/app/search/hooks/search.tsx
@@ -0,0 +1,61 @@
+'use client'
+import { appCtx } from '@/app/data'
+import { Country } from '@/app/data/types'
+import { useContext } from 'react'
+
+declare global {
+  interface String {
+    lcStartsWith(
+      searchString: string,
+      position?: number | undefined,
+    ): boolean
+  }
+}
+
+String.prototype.lcStartsWith = function (
+  searchString: string,
+  position?: number | undefined,
+): boolean {
+  return this.toLowerCase().indexOf(searchString) === 0
+}
+
+export const useSearch = () => {
+  const { data, _: { setResults } } = useContext(appCtx)
+
+  const filter = (query: string): void => {
+    if (!query.length || query.match(/[^a-zA-Z ]/g)?.length) {
+      setResults([])
+      return
+    }
+
+    const lcQuery = query.toLowerCase()
+    const specificCountry: [string, Country] | undefined
+      = Object.entries(data).find(
+        ([countryCode, details]: [string, Country]) => {
+          return query.toUpperCase() === countryCode
+            || query.toLowerCase() === details.name.toLowerCase()
+        }
+      )
+
+    if (specificCountry) {
+      const [_, country] = specificCountry
+      setResults([country])
+      return
+    }
+
+    const options: Country[] = Object.entries(data).filter(
+      ([_, details]: [string, Country]) => {
+        const { name, code } = details
+
+        return name.lcStartsWith(lcQuery) || code.lcStartsWith(lcQuery)
+      }
+    ).map(([_, details]: [string, Country]) => details)
+
+    setResults(options)
+    return
+  }
+
+  return {
+    filter,
+  }
+}
