fde068cd44320e12925f21078b3bab1c4118eb4d
Ross Libby <dev@rosslibby.com>
2024-01-26 21:31:54 -0500

    reduced props

--- src/app/search/loading/index.tsx
+++ src/app/search/loading/index.tsx
@@ -1,8 +1,10 @@
+import { useContext } from 'react'
 import styles from './loading.module.css'
+import { appCtx } from '@/app/data'
 
-export default function Loading({ loading }: {
-  loading: boolean
-}) {
+export default function Loading() {
+  const { loading } = useContext(appCtx)
+
   return loading ? (
     <div className={styles.ring}>
       <div />
--- src/app/search/submit.tsx
+++ src/app/search/submit.tsx
@@ -1,16 +1,19 @@
+import { useContext } from 'react'
+import { useApi } from './hooks'
 import styles from './search.module.css'
+import { appCtx } from '../data'
 
-export default function SubmitButton({ loading, submit, value }: {
-  loading: boolean
-  submit: () => Promise<void>
+export default function SubmitButton({ value }: {
   value: string
 }) {
+  const { loading } = useContext(appCtx)
+  const { submit } = useApi()
   return (
     <button
       className={styles.searchButton}
       disabled={value.length !== 2 || !value.match(/[A-Z]{2}/g) || loading}
       id="submit"
-      onClick={submit}
+      onClick={() => submit(value)}
     >
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
         <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
