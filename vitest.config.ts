c6ab6bbd1e1e7f6acd0a998c5e6bcbf60c75d795
Ross Libby <dev@rosslibby.com>
2024-01-26 21:52:45 -0500

    add vite UI

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
  
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
+    globals: true,
     environment: 'jsdom',
   },
 })
